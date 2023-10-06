7bffa48b301dd389235fa184cb14555821e8e959
SandersonHub <128574459+SandersonHub@users.noreply.github.com>
2023-10-06 18:31:32 -0400

    update

--- server/server.js
+++ server/server.js
@@ -1,46 +1,45 @@
 const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
-const { authMiddleware } = require('./utils/auth');
-
-const { typeDefs, resolvers } = require('./schemas');
+const { ApolloServer } = require('apollo-server-express');
 const db = require('./config/connection');
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// apolloServer setup
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware,
 });
 
-app.use(express.urlencoded({ extended: false }));
+// middleware for parsing JSON and urlencoded form data
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Serve up static assets
+// foir heroku
 app.use('/images', express.static(path.join(__dirname, '../client/images')));
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-
-// Create a new instance of an Apollo server with the GraphQL schema
-const startApolloServer = async () => {
-  await server.start();
+// starts appolo server
+server.start().then(() => {
   server.applyMiddleware({ app });
-  
+
+  // connect to the MongoDB and then it will start express.js
   db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
       console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-    })
-  })
-  };
-  
-// Call the async function to start the server
-  startApolloServer();
+    });
+  });
+});
+
