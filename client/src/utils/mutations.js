bf2a287ed15f65e5ef25a70a79dc781f34485396
unknown <peter.ostertag99@gmail.com>
2023-10-10 21:07:20 -0500

    Add mutation.js

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,2 +1,21 @@
 import { gql } from '@apollo/client';
 
+export const SAVE_GAME = gql`
+    mutation saveGame($_id: ID!, $name: String!, $console: GameConsole) {
+        saveGame(_id: $_id, name: $name, console: $console) {
+            _id
+            name
+            console
+        }
+    }
+`;
+
+export const REMOVE_GAME = gql`
+    mutation removeGame($_id: ID!, $name: String!, $console: GameConsole) {
+        saveGame(_id: $_id, name: $name, console: $console) {
+            _id
+            name
+            console
+        }
+    }
+`;
