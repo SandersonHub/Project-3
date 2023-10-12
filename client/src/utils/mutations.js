4f347fe36c0fce6945af797e6017fa644f575ef9
jfujan <jfujan@outlook.com>
2023-10-12 18:55:07 -0500

    mutations finished

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,5 +1,29 @@
 import { gql } from '@apollo/client';
 
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
 export const SAVE_GAME = gql`
     mutation saveGame($_id: ID!, $name: String!, $console: GameConsole) {
         saveGame(_id: $_id, name: $name, console: $console) {
