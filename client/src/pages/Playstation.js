18940ed31ebabe2ebcce803a82c6d33923a9781f
SandersonHub <128574459+SandersonHub@users.noreply.github.com>
2023-10-15 14:40:21 -0400

    pages-games update, gameSeeds.json test (2 images)

---
+++ client/src/pages/Playstation.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { useQuery } from '@apollo/client';
+import { QUERY_CONSOLE } from '../utils/queries';
+
+const PlayStation = () => {
+  // querying the games specific to the "PS5" console
+  const { loading, data, error } = useQuery(QUERY_CONSOLE, {
+        variables: { consoleType: "PS5" },
+    });
+
+    // loading 
+    if (loading) {
+        return <h2>Loading games...</h2>;
+    }
+
+    // error handling
+    if (error) {
+        console.error("Error fetching PS5 games: ", error);
+        return <h2>Error loading games.</h2>;
+    }
+
+    // extracting the games data from the query
+    const games = data?.games || [];
+
+    // Rendering the list of games for the "PS5" console.
+    return (
+        <div>
+            <h1>PlayStation Games</h1>
+            {games.length ? (
+                <div>
+                    {games.map((game) => (
+                        <div key={game._id}>
+                            <h3>{game.name}</h3>
+                        </div>
+                    ))}
+                </div>
+            ) : (
+                <h4>No games found for PlayStation</h4>
+            )}
+        </div>
+    );
+};
+
+export default PlayStation;
---
+++ client/src/pages/Switch.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { useQuery } from '@apollo/client';
+import { QUERY_CONSOLE } from '../utils/queries';
+
+const Switch = () => {
+  // querying the games specific to the "Switch" console
+  const { loading, data } = useQuery(QUERY_CONSOLE, {
+    variables: { consoleType: "Switch" }
+  });
+
+  // loading
+  if (loading) {
+    return <h2>Loading...</h2>;
+  }
+
+  // extracting the games data from the query response
+  const games = data?.games || [];
+
+  return (
+    <div>
+      <h1>Nintendo Switch Games</h1>
+      {games.length ? (
+        <div>
+          {games.map((game) => (
+            <div key={game._id}>
+              <h3>{game.name}</h3>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <h4>No games available for this console</h4>
+      )}
+    </div>
+  );
+};
+
+export default Switch;
